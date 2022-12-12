685dac034c84690e42ca90d90ebb7d084a4cc9f0
Евгений <msk9199625687@yandex.ru>
2022-12-12 21:07:12 +0300

    sass

---
+++ src/js/data.js
@@ -0,0 +1,49 @@
+const data = [{
+    id: 1,
+    images: ['/src/images/products/tatran-s.jpg', '/src/images/products/tatran-m.jpg'],
+    title: 'Кровать TATRAN',
+    info: 'Основание из полированной нержавеющей стали, придает оригинальный парящий эффект.',
+    price: '120 000',
+    min: 1,
+    max: 5
+}, {
+    id: 2,
+    images: ['/src/images/products/vilora-s.jpg', '/src/images/products/vilora-m.jpg'],
+    title: 'Кресло VILORA',
+    info: 'Мягкое и уютное, аккуратное и стильное. Упругие подушки сиденья и приятная на ощупь ткань. ',
+    price: '21 000',
+    min: 2,
+    max: 8
+}, {
+    id: 3,
+    images: ['/src/images/products/menu-s.jpg', '/src/images/products/menu-m.jpg'],
+    title: 'Стол MENU',
+    info: 'Европейский дуб - отличается особой прочностью и стабильностью.',
+    price: '34 000',
+    min: 1,
+    max: 2
+}, {
+    id: 4,
+    images: ['/src/images/products/askesta-s', '/src/images/products/askesta-m'],
+    title: 'Диван ASKESTA',
+    info: 'Благодаря защелкивающемуся механизму диван легко раскладывается в комфортную кровать',
+    price: '68 000',
+    min: 1,
+    max: 9
+}, {
+    id: 5,
+    images: ['/src/images/products/lunar-s', '/src/images/products/lunar-m'],
+    title: 'Кресло LUNAR',
+    info: 'Прекрасно переносит солнечные лучи, перепады влажности и любые осадки',
+    price: '40 000',
+    min: 2,
+    max: 6
+}, {
+    id: 6,
+    images: ['/src/images/products/nastan-s', '/src/images/products/nastan-m'],
+    title: 'Шкаф Nastan',
+    info: 'Мебель может быть оснащена разнообразными системами подсветки.',
+    price: '80 000',
+    min: 1,
+    max: 2
+}, ]
