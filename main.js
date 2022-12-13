65a2827c04e39335f4d124bbf5dfdf8ea4626642
Евгений <msk9199625687@yandex.ru>
2022-12-13 19:14:04 +0300

    v.1.0.0

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     "use strict";
     const e = [{
         id: 1,
-        images: ["/images/products/tatran-s.jpg", "/images/products/tatran-m.jpg"],
+        images: ["images/products/tatran-s.jpg", "images/products/tatran-m.jpg"],
         title: "Кровать TATRAN",
         info: "Основание из полированной нержавеющей стали, придает оригинальный парящий эффект.",
         price: 12e4,
@@ -10,7 +10,7 @@
         max: 5
     }, {
         id: 2,
-        images: ["/images/products/vilora-s.jpg", "/images/products/vilora-m.jpg"],
+        images: ["images/products/vilora-s.jpg", "images/products/vilora-m.jpg"],
         title: "Кресло VILORA",
         info: "Мягкое и уютное, аккуратное и стильное. Упругие подушки сиденья и приятная на ощупь ткань. ",
         price: 21e3,
@@ -18,7 +18,7 @@
         max: 8
     }, {
         id: 3,
-        images: ["/images/products/menu-s.jpg", "/images/products/menu-m.jpg"],
+        images: ["images/products/menu-s.jpg", "images/products/menu-m.jpg"],
         title: "Стол MENU",
         info: "Европейский дуб - отличается особой прочностью и стабильностью.",
         price: 34e3,
@@ -26,7 +26,7 @@
         max: 2
     }, {
         id: 4,
-        images: ["/images/products/askesta-s.jpg", "/images/products/askesta-m.jpg"],
+        images: ["images/products/askesta-s.jpg", "images/products/askesta-m.jpg"],
         title: "Диван ASKESTA",
         info: "Благодаря защелкивающемуся механизму диван легко раскладывается в комфортную кровать",
         price: 68e3,
@@ -34,7 +34,7 @@
         max: 9
     }, {
         id: 5,
-        images: ["/images/products/lunar-s.jpg", "/images/products/lunar-m.jpg"],
+        images: ["images/products/lunar-s.jpg", "images/products/lunar-m.jpg"],
         title: "Кресло LUNAR",
         info: "Прекрасно переносит солнечные лучи, перепады влажности и любые осадки",
         price: 4e4,
@@ -42,7 +42,7 @@
         max: 6
     }, {
         id: 6,
-        images: ["/images/products/nastan-s.jpg", "/images/products/nastan-m.jpg"],
+        images: ["images/products/nastan-s.jpg", "images/products/nastan-m.jpg"],
         title: "Шкаф Nastan",
         info: "Мебель может быть оснащена разнообразными системами подсветки.",
         price: 8e4,
